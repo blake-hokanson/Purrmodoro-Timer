df1f8971d5c35acb5ce33d9539236bfb5efaba1c
Brooklyn Cooper <brooklyn.cooper.mail@gmail.com>
2023-04-22 08:44:57 -0500

    Added font files

--- chrome/block.js
+++ chrome/block.js
@@ -6,17 +6,29 @@ console.log("Loaded");
 let prohib = false;
 for (let site in sites) {
   let regex = new RegExp(sites[site]); // Regex to check if url is a sub-site of one of the prohibited site
-  console.log(regex)
+  console.log(regex);
   // Check if prohibited site regex matches current url
   if (regex.test(window.location.href)) {
-    console.log("On prohibited website")
-    prohib = true
-    let body = document.body;
-    // Update page with error message and new formatting
-    body.innerHTML= "<h1 id='message'>This is a prohibited page!</h1>"
-    body.parentElement.style["backgroundColor"] = "purple";
-    body.style["backgroundColor"] = "purple";
-    body.style["color"] = "white";
+    console.log("On prohibited website");
+    prohib = true;
+    document.open();
+    document.write(`<!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+    <body>
+    
+        <div id="message">Nice try! Get back to work.</div>
+    
+    </body>
+    </html>`);
+    document.close();
+    // let body = document.body;
+    // // Update page with error message and new formatting
+    // body.innerHTML= "<h1 id='message'>This is a prohibited page!</h1>"
+    // body.parentElement.style["backgroundColor"] = "purple";
+    // body.style["backgroundColor"] = "purple";
+    // body.style["color"] = "white";
   }
 }
 if (!prohib) {
