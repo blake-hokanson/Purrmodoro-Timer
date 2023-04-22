2708110b1ed7caafbd43a3b062b0ac16c5e29f8b
blake-hokanson <blake.hokanson@icloud.com>
2023-04-21 23:21:48 -0700

    Removed all extra files

--- packages/backend/__main__.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-import backend.app as app
-import os
-
-uvicorn.run(app.app, host="0.0.0.0", port=int(os.environ.get("BACKEND_PORT", 8000)))
--- packages/frontend/theme.ts
+++
@@ -1,5 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-export default extendTheme({
-  initialColorMode: "light",
-});
