6dafdb19e9725794e548fb6927e0937b02e0d516
Brooklyn Cooper <brooklyn.cooper.mail@gmail.com>
2023-04-22 02:18:06 -0500

    added files with basic functionality to block certain files

---
+++ chrome/block.js
@@ -0,0 +1,24 @@
+// Create array of prohibited sites (later replace with user defined sites?? Add functionality to check if on break?)
+let sites = [".netflix.com", ".youtube.com",".twitter.com",".instagram.com"];
+console.log("Loaded");
+
+// Loop through prohibited sites, check if current site matches any of them
+let prohib = false;
+for (let site in sites) {
+  let regex = new RegExp(sites[site]); // Regex to check if url is a sub-site of one of the prohibited site
+  console.log(regex)
+  // Check if prohibited site regex matches current url
+  if (regex.test(window.location.href)) {
+    console.log("On prohibited website")
+    prohib = true
+    let body = document.body;
+    // Update page with error message and new formatting
+    body.innerHTML= "<h1 id='message'>This is a prohibited page!</h1>"
+    body.parentElement.style["backgroundColor"] = "purple";
+    body.style["backgroundColor"] = "purple";
+    body.style["color"] = "white";
+  }
+}
+if (!prohib) {
+  console.log("This site is fine :)")
+}
