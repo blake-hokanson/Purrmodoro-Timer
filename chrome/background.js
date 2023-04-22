e47e61e04950ff379d262bb019b8c167ee9fb603
blake-hokanson <blake.hokanson@icloud.com>
2023-04-22 10:45:14 -0700

    Added padding for numbers, added perfect sync between timers

--- chrome/background.js
+++ chrome/background.js
@@ -1,18 +1,25 @@
 let isRunning = false; //function that loops
-let countDownTime; //time started
-let timerMin = 15; //min when started
-let timerSec = 0; //sec when started
+let countDownTime; //date and time that timer ends
+let timerMin = 15; //min when started on timer
+let timerSec = 0; //sec when started on timer
 
 const setTimer = (msg) => {
+  //sets a time for the timer
   if (msg !== null && msg.msg === "set") {
     timerMin = msg.min;
     timerSec = msg.sec;
-    sendMsg("setMsg");
+    if (isRunning) {
+      sendMsg("stopMsg");
+      startTimer({ msg: "start" });
+    } else {
+      sendMsg("setMsg");
+    }
   }
 };
 chrome.runtime.onMessage.addListener(setTimer);
 
 const startTimer = (msg) => {
+  //starts all timers
   if (msg !== null && msg.msg === "start") {
     isRunning = true;
     countDownTime =
@@ -23,30 +30,18 @@ const startTimer = (msg) => {
 chrome.runtime.onMessage.addListener(startTimer);
 
 const stopTimer = (msg) => {
+  //stops all timers
   if (msg !== null && msg.msg === "stop") {
     isRunning = false;
     sendMsg("stopMsg");
-    timerMin = Math.floor((msg.time % (1000 * 60 * 60)) / (1000 * 60));
-    timerSec = Math.floor((msg.time % (1000 * 60)) / 1000);
+    const time = countDownTime - new Date().getTime();
+    [timerMin, timerSec] = getMinSec(time);
   }
 };
 chrome.runtime.onMessage.addListener(stopTimer);
 
 const sendMsg = (msg, min = timerMin, sec = timerSec) => {
-  /* not needed
-  chrome.tabs.query(
-    { url: "chrome-extension://" + "*" + "/*" }, // could replace "*" with chrome.runtime.id
-    (tabs) => {
-      for (let tab of tabs) {
-        chrome.tabs.sendMessage(tab.id, {
-          msg: msg,
-          min: min,
-          sec: sec,
-        });
-      }
-    }
-  );
-  */
+  //sends given message and data to all extension tabs
   chrome.runtime.sendMessage({
     msg: msg,
     min: min,
@@ -54,19 +49,41 @@ const sendMsg = (msg, min = timerMin, sec = timerSec) => {
   });
 };
 
-sendData = (msg, sender, sendResponse) => {
+addTimer = (msg, sender, sendResponse) => {
+  //sends data when new timer is added and then syncs them all
   if (msg !== null && msg.msg === "timerLoaded") {
-    sendResponse({
-      countDownTime: countDownTime,
-      isRunning: isRunning,
-      min: timerMin,
-      sec: timerSec,
-    });
+    if (!isRunning) {
+      sendResponse({
+        countDownTime: countDownTime,
+        isRunning: isRunning,
+        min: timerMin,
+        sec: timerSec,
+      });
+    } else {
+      const time = countDownTime - new Date().getTime();
+      const [min, sec] = getMinSec(time);
+      sendResponse({
+        countDownTime: countDownTime,
+        isRunning: isRunning,
+        min: min,
+        sec: sec,
+      });
+      stopTimer({ msg: "stop" });
+      startTimer({ msg: "start" });
+    }
   }
 };
-chrome.runtime.onMessage.addListener(sendData);
+chrome.runtime.onMessage.addListener(addTimer);
+
+getMinSec = (time) => {
+  //converts time into mins and secs
+  min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  sec = Math.floor((time % (1000 * 60)) / 1000);
+  return [min, sec];
+};
 
 isRunningSend = (msg, sender, sendResponse) => {
+  //returns if the timers are running or notlog
   if (msg !== null && msg.msg === "isRunning") {
     sendResponse(isRunning);
   }
